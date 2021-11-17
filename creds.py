5ffb8d47f117499979ab1b2dca055f8e155cfe26
adtor97 <adrian135torrejon@gmail.com>
2021-11-17 14:37:20 -0500

    fix emails creds

--- creds.py
+++ creds.py
@@ -1,5 +1,5 @@
 users = [
         {"username":"at@digitaliatec.com", "password":"$2b$13$WSMCijCz/BcQpugXZYIJY.2T./jpYr8M4XczozbJZwCBleDQ4/am.", "name":"Adrian", "views":["admin"]},
-        {"username":"raul.delmar@digitaliatec.com ", "password":"$2b$13$WSMCijCz/BcQpugXZYIJY.2T./jpYr8M4XczozbJZwCBleDQ4/am.", "name":"Raul", "views":["tickets"]},
-        {"username":"cesar@digitaliatec.com ", "password":"$2b$13$WSMCijCz/BcQpugXZYIJY.2T./jpYr8M4XczozbJZwCBleDQ4/am.", "name":"Cesar", "views":["tickets"]},        
+        {"username":"raul.delmar@digitaliatec.com", "password":"$2b$13$WSMCijCz/BcQpugXZYIJY.2T./jpYr8M4XczozbJZwCBleDQ4/am.", "name":"Raul", "views":["tickets"]},
+        {"username":"cesar@digitaliatec.com", "password":"$2b$13$WSMCijCz/BcQpugXZYIJY.2T./jpYr8M4XczozbJZwCBleDQ4/am.", "name":"Cesar", "views":["tickets"]},        
         ]
