74f03ce8933acfef3beaffc6ee8e48e4c6fd2021
adtor97 <adrian135torrejon@gmail.com>
2021-11-17 14:32:47 -0500

    first commit heroku

--- dashboards/admin.py
+++ dashboards/admin.py
@@ -9,7 +9,6 @@ import pandas as pd
 import requests
 
 def serve_layout():
-
     layout = html.Div([
                         html.Div(id='none',children=[],style={'display': 'none'})
                         , dbc.Col(
